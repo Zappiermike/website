56d8dc0e73a36c2075ea471e3e2cfb078345120c
Michael Scheie <mscheie@Michaels-MacBook-Air.local>
2023-02-15 22:24:16 -0600

    Introducing react

--- src/app.py
+++ src/app.py
@@ -2,13 +2,11 @@
 
 import flask
 import os
-import sys
 
 
 os.environ["FLASK_APP"] = str(__name__)
-# os.environ["FLASK_DEBUG"] = "true"
-app = flask.Flask(__name__, template_folder="../templates")
-app.debug = True
+os.environ["FLASK_DEBUG"] = "true"
+app = flask.Flask(__name__, template_folder="../templates/", static_folder="../static/")
 
 
 
