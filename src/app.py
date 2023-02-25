804099e263d9a8fe4321b5a9fb147d03f1778864
Michael Scheie <mscheie@Michaels-MacBook-Air.local>
2023-02-25 10:14:45 -0600

    Created separate pages and linked navigation

--- src/app.py
+++ src/app.py
@@ -3,13 +3,18 @@
 import flask
 import os
 
-
 os.environ["FLASK_APP"] = str(__name__)
 os.environ["FLASK_DEBUG"] = "true"
 app = flask.Flask(__name__, template_folder="../templates/", static_folder="../static/")
 
-
-
 @app.route("/")
-def hello_world():
+def render_homepage():
     return flask.render_template("homepage.html")
+
+@app.route("/about")
+def render_about():
+    return flask.render_template("about.html")
+
+@app.route("/projects")
+def render_projects():
+    return flask.render_template("projects.html")
