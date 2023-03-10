f4c2195d573f46116866593e87bfe430f932d3e5
Michael Scheie <mscheie@Michaels-MacBook-Air.local>
2023-03-09 22:20:53 -0600

    Fixed root path for flask

--- src/app.py
+++ src/app.py
@@ -2,10 +2,13 @@
 
 import flask
 import os
+import json
 
 os.environ["FLASK_APP"] = str(__name__)
 os.environ["FLASK_DEBUG"] = "true"
-app = flask.Flask(__name__, template_folder="../templates/", static_folder="../static/")
+# app = flask.Flask(__name__, template_folder="../templates/", static_folder="../static/")
+app = flask.Flask(__name__, root_path=os.path.abspath(os.path.join(os.getcwd(), '..')))
+print(app.root_path)
 
 @app.route("/")
 def render_homepage():
@@ -18,3 +21,13 @@ def render_about():
 @app.route("/projects")
 def render_projects():
     return flask.render_template("projects.html")
+
+@app.route("/oats")
+def retrieve_github_token():
+    with open("static/tokens.json") as f:
+        data = json.load(f)
+        token = data.get("github", None)
+        if token is not None:
+            return token
+        else:
+            return "None"
